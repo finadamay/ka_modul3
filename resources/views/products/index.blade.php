551aaf423453aaa95efa9fed2b559863936d60f6
LEAA <serli_april@student.ittelkom-sby.ac.id>
2024-06-12 21:46:22 +0700

    Update product page

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -1,5 +1,20 @@
 @extends('layouts.app')
 
 @section('content')
-Data Produk
+    <div class="container">
+        <div class="row">
+            @foreach ($products as $product)
+                <div class="col-md-4">
+                    <div class="card">
+                        <img src="{{$product->image}}" alt="" class="card-img-top">
+                        <div class="card-body">
+                            <h5 class="card-title">{{$product->title}}</h5>
+                            <h2>$ {{$product->price}}</h2>
+                            <p class="card-text">In Stock: {{$product->in_stock}}</p>
+                        </div>
+                    </div>
+                </div>
+            @endforeach
+        </div>
+    </div>
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -29,7 +29,40 @@ Route::get('/home', function () {
     return view('welcome');
 });
 
-Route::view('products', 'products.index')->name('products.index');
+Route::view('products', 'products.index', [
+    'products' => [
+       (object) [
+        'title' => 'Test',
+        'image' => 'https://images.unsplash.com/photo-1541643600914-78b084683601?q=80&w=1408&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+        'price' => 999,
+        'in_stock' => 23
+       ],
+       (object) [
+        'title' => 'Versace Eros',
+        'image' => 'https://images.unsplash.com/photo-1587017539504-67cfbddac569?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+        'price' => 123,
+        'in_stock' => 12
+       ],
+       (object) [
+        'title' => 'Test3',
+        'image' => 'https://images.unsplash.com/photo-1594035910387-fea47794261f?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+        'price' => 567,
+        'in_stock' => 65
+       ],
+       (object) [
+        'title' => 'Versace Eros',
+        'image' => 'https://images.unsplash.com/photo-1587017539504-67cfbddac569?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+        'price' => 123,
+        'in_stock' => 12
+       ],
+       (object) [
+        'title' => 'Test3',
+        'image' => 'https://images.unsplash.com/photo-1594035910387-fea47794261f?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+        'price' => 567,
+        'in_stock' => 65
+       ],
+    ]
+])->name('products.index');
 
 // Route::get('/home', [LoginController::class, 'showLoginForm']);
 Route::post('/login', [LoginController::class, 'processLogin'])->name('login');
